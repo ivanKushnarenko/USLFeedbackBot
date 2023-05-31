f7b22f1eec044381c20ac908a33cac8b4e5c42a5
Ivan Kushnarenko <ivan.kushnarenko@gmdhshell.com>
2023-05-31 21:23:58 +0300

    Auth fixed

--- src/utils.py
+++ src/utils.py
@@ -8,13 +8,11 @@ from user import UserInfo
 
 
 def answers_str(answers: list[str], cmd_type: CommandType, user: telebot.types.User) -> str:
-    username: str = user.username
-    full_name: str = user.full_name
+    username: str = '@' + user.username if user.username else user.full_name
     user_info: UserInfo = auth.user_info(user.id)
     if user_info is not None:
-        username = user_info.username
-        full_name = user_info.full_name
-    user_ref: str = "@" + username if username else f"[{full_name}](tg://user?id={user.id})"
+        username = '@' + user_info.username if user_info.username else user_info.full_name
+    user_ref: str = f'[{username}](tg://user?id={user.id})'
     res: str = msg.chat_message(cmd_type).format(user_ref, *answers)
     if user_info and user_info.description:
         res += f'\n\n_Автор звернення:_\n*{user_info.description}*'
